4116bea6208a642a9217639fc836936d779e7b76
arqarq <43341071+arqarq@users.noreply.github.com>
2021-05-30 16:18:15 +0200

    files added

---
+++ groovy-learn2.groovy
@@ -0,0 +1,11 @@
+println 3.5.getClass().getName()
+println 2.0f - 1.1f
+println 2.0d - 1.1d
+println 2.0 - 1.1
+println 2.0.minus(1.1)
+println 3/5
+println 3.intdiv(5)
+'aaa'
+"aaa ${1 + 1}"
+def d =  'this is a string'
+d[0..3,5..6,8..8,10..-1]
