89f3f976ce9fb8de4249f1cc4beb1d3164bada7a
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2021-07-30 12:01:05 +0200

    * flickr

--- sources/groovy/flickr/gallery.groovy
+++ sources/groovy/flickr/gallery.groovy
@@ -1,4 +1,39 @@
 package flickr
 
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovy.swing.SwingBuilder
+
+import javax.swing.*
+import java.awt.*
+
 final String key = new File('flickr_key.txt').text
-println key
+final String endPoint = 'https://api.flickr.com/services/rest?'
+paramsAndQsAndJson = [method: 'flickr.photos.search', api_key: key, format: 'json', tags: 'cat,dogs', nojsoncallback: 1,
+                      media : 'photos', per_page: 6]
+paramsAndQsAndJson = paramsAndQsAndJson.collect { k, v -> "$k=$v" }.join('&')
+paramsAndQsAndJson = "$endPoint$paramsAndQsAndJson".toURL().text
+
+final f = new File('catsAndDogs.json')
+if (f) {
+    f.delete()
+}
+f << JsonOutput.prettyPrint(paramsAndQsAndJson)
+println JsonOutput.prettyPrint(paramsAndQsAndJson)
+paramsAndQsAndJson = new JsonSlurper().parseText(paramsAndQsAndJson)
+photos = paramsAndQsAndJson.photos.photo
+photos.each {
+    println(paramsAndQsAndJson = "https://farm${it.farm}.staticflickr.com/${it.server}/${it.id}_${it.secret}.jpg")
+    it.bytesFromUrl = paramsAndQsAndJson.toURL().getBytes()
+}
+new SwingBuilder().edt {
+    final frame = frame(title: 'Cat and dog pictures', visible: false, pack: true, defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE,
+            layout: new GridLayout(0, 2, 2, 2)) {
+        photos.each { final p ->
+            label(icon: new ImageIcon(p.bytesFromUrl), toolTipText: p.title)
+        }
+    }
+    frame.setLocation(30, 30)
+    frame.setVisible(true) // no small, unprepared window
+    frame.requestFocus() // blinking on taskbar
+}
