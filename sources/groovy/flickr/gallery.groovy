1a7baf22f6fe9bae5eeee3653327e6982cbc3fe5
Arkadiusz Sekuła <arkadiusz.sekula@comarch.pl>
2021-07-30 13:39:18 +0200

    * flickr

--- sources/groovy/flickr/gallery.groovy
+++ sources/groovy/flickr/gallery.groovy
@@ -9,15 +9,13 @@ import java.awt.*
 
 final String key = new File('flickr_key.txt').text
 final String endPoint = 'https://api.flickr.com/services/rest?'
-paramsAndQsAndJson = [method: 'flickr.photos.search', api_key: key, format: 'json', tags: 'cat,dogs', nojsoncallback: 1,
+paramsAndQsAndJson = [method: 'flickr.photos.search', api_key: key, format: 'json', tags: 'cat,dog', nojsoncallback: 1,
                       media : 'photos', per_page: 6]
 paramsAndQsAndJson = paramsAndQsAndJson.collect { k, v -> "$k=$v" }.join('&')
 paramsAndQsAndJson = "$endPoint$paramsAndQsAndJson".toURL().text
 
 final f = new File('catsAndDogs.json')
-if (f) {
-    f.delete()
-}
+deleteFileIfExists(f)
 f << JsonOutput.prettyPrint(paramsAndQsAndJson)
 println JsonOutput.prettyPrint(paramsAndQsAndJson)
 paramsAndQsAndJson = new JsonSlurper().parseText(paramsAndQsAndJson)
@@ -36,4 +34,11 @@ new SwingBuilder().edt {
     frame.setLocation(30, 30)
     frame.setVisible(true) // no small, unprepared window
     frame.requestFocus() // blinking on taskbar
+}
+deleteFileIfExists(f)
+
+private static void deleteFileIfExists(final File f) {
+    if (f) {
+        f.delete()
+    }
 }
